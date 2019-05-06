aaa26819a53bd8126b31d6f3e53d757ba8a15d38
christophcannz <christophcannz@gmail.com>
2019-05-06 15:18:57 -0500

    mondayaft

--- assets/JavaScript/giphy.js
+++ assets/JavaScript/giphy.js
@@ -10,7 +10,7 @@
 function getGiphy() {
   // fetch giphy data from the giphy API.
   var searchTerm = document.querySelector("#search").value
-  var testUrl ="https://api.giphy.com/v1/gifs/search?api_key=OsQF2Dn1DvyD1rk1wcwzxYUotxkeDDXD&limit=10&q="+searchTerm
+  var testUrl ="https://api.giphy.com/v1/gifs/search?q= + topic + &api_key=OsQF2Dn1DvyD1rk1wcwzxYUotxkeDDXD&limit=10&q="+searchTerm
 
   fetch (testUrl)
   .then(data => data.json())
@@ -23,43 +23,21 @@ function getGiphy() {
     var giphyLink = firstItem.images.fixed_width.url
     document.querySelector("#gif").setAttribute("src", giphyLink);
   })
+
+  
   // Pick a gif from the search term.
   // make that gif appear on the page.
   $.ajax({
   url: testUrl,
   method: "GET"
-}) 
-
-.then(function(response) {
-
-  //
-    var imageUrl = response.data.image_original_url;
-    console.log(imageUrl);
-
-    //
-    var squirrelImage = $("<img>");
-
-    //
-    squirrelImage.attr("src", imageUrl);
-    squirrelImage.attr("alt", "squirrel image");
-
-    //
-    $("#images").prepend(squirrelImage);
-});
+  }) 
 
 }
 
-/*$(document).ready(function(){
+$(document).ready(function(){
   var animals = ["squirrel", "chipmunk", "rabbit"];
   for (var i = 0; i< animals.length; i++) {
-      $("#squirrel").on("click", function() {
-
-      })
-
+      var buttons = $('<button>' + animals[i] + '</button>')
+      buttons.appendTo('#buttons');
   }
-})*/
-
-
-
-//
- 
+})
